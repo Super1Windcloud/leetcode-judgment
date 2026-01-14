6593a9d79cbb0a8115e179041bf3362aa43434c3
SuperWindCloud <19338823723@163.com>
2026-01-14 15:17:53 +0800

    refactor: clean up code formatting and improve consistency in NextAuth configuration

--- lib/nextauth.config.ts
+++ lib/nextauth.config.ts
@@ -1,44 +1,48 @@
-import type { Account, Session } from "next-auth";
-import type { JWT } from "next-auth/jwt";
+import type {Account, AuthOptions, Session} from "next-auth";
+import type {JWT} from "next-auth/jwt";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 declare module "next-auth/jwt" {
-	interface JWT {
-		accessToken?: string;
-	}
+    interface JWT {
+        accessToken?: string;
+    }
 }
 
 declare module "next-auth" {
-	interface Session {
-		accessToken?: string;
-	}
+    interface Session {
+        accessToken?: string;
+    }
 }
 
-export const NextAuthConfig = {
-	providers: [
-		GitHubProvider({
-			clientId: process.env.AUTH_GITHUB_ID ?? "",
-			clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
-		}),
-		GoogleProvider({
-			clientId: process.env.AUTH_GOOGLE_ID ?? "",
-			clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
-		}),
-	],
-	callbacks: {
-		async jwt({ token, account }: { token: JWT; account: Account | null }) {
-			// Persist the OAuth access_token to the token right after signin
-			if (account) {
-				token.accessToken = account.access_token;
-			}
-			return token;
-		},
-		async session({ session, token }: { session: Session; token: JWT }) {
-			// Send properties to the client, like an access_token from a provider
-			session.accessToken = token.accessToken;
-			return session;
-		},
-	},
-	secret: process.env.AUTH_SECRET,
+export const NextAuthConfig: AuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.AUTH_GITHUB_ID ?? "",
+            clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
+        }),
+        GoogleProvider({
+            clientId: process.env.AUTH_GOOGLE_ID ?? "",
+            clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+        }),
+    ],
+    callbacks: {
+        async jwt({token, account}: { token: JWT; account: Account | null }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({session, token}: { session: Session; token: JWT }) {
+            // Send properties to the client, like an access_token from a provider
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
+    secret: process.env.AUTH_SECRET,
+    session: {
+        strategy: "jwt",
+        maxAge: 7 * 24 * 60 * 60,
+    }
 };
